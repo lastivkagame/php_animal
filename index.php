f50375a1ec5ef13f54c3d46597c6409b36214b7c
Annonymous <dev@rivne.itstep.org>
2021-05-22 19:16:39 +0300

    second

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
     //      echo "<p>Hello my class! {$c} </p>";
     // ?>
 
-    <button class="btn btn-warning" ><a style="text-decoration:none;" href="add.php"> ADD Animal</a></button>
 
 <?php include "data_table.php"; ?>
+<div class="container d-flex w-100">
+<button class="btn btn-warning" ><a style="text-decoration:none;color: black;width: 100%" href="add.php"> ADD Animal</a></button>
+</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
---
+++ js/app.js
@@ -0,0 +1,3 @@
+function NotReload(e){
+    e.preventDefault();
+}
